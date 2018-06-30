fe74bf461ece5067f2221085d4fcc1542d944c2d
AnimatorJoe <josephjin7@gmail.com>
2018-06-29 23:16:14 -0500

    Initial Commit

---
+++ Loc-Sim/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Loc-Sim
+//
+//  Created by Joseph Jin on 6/29/18.
+//  Copyright © 2018 AnimatorJoe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Loc-SimTests/Loc_SimTests.swift
@@ -0,0 +1,36 @@
+//
+//  Loc_SimTests.swift
+//  Loc-SimTests
+//
+//  Created by Joseph Jin on 6/29/18.
+//  Copyright © 2018 AnimatorJoe. All rights reserved.
+//
+
+import XCTest
+@testable import Loc_Sim
+
+class Loc_SimTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
