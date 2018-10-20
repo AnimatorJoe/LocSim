e0e8efde22ca618c6ee69cfe7ab0ec5eaa8bb5f8
AnimatorJoe <josephjin7@gmail.com>
2018-10-19 21:19:15 -0500

    Adding files

--- Loc-Sim/ViewController.swift
+++ Loc-Sim/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet var map: MKMapView!
+    let manager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestAlwaysAuthorization()
+        manager.startUpdatingLocation()
+        
+    }
+    
+    // Extracting Location
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let loc = locations[0]
+        
+        let span: MKCoordinateSpan = MKCoordinateSpanMake(10, 10) // Zoom
+        let currLoc: CLLocationCoordinate2D = CLLocationCoordinate2DMake(loc.coordinate.latitude, loc.coordinate.longitude) // Location
+        let region: MKCoordinateRegion = MKCoordinateRegionMake(currLoc, span) // Set region
+        map.setRegion(region, animated: true) // Update map
+        
+        self.map.showsUserLocation = true // Show blue dot
     }
 
     override func didReceiveMemoryWarning() {
